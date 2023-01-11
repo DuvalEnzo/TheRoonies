3399c2505462ef1d1bd80e225b0b67ee06c0b544
Clément Addario <71002854+ClementAdd@users.noreply.github.com>
2023-01-11 17:09:06 +0100

    Add img

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use('/static', express.static('src'));
+app.use(express.static(__dirname + '/'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/src/home/index.html');
 })
+
+//add img route
+
+
+
 app.listen(port, () => {
     console.log(`http://localhost:${port}`)
 })
+
